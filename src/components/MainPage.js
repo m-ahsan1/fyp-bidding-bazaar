1ee6396c83489b0ece8f1d7ec8472ec15b52d765
Abdullah-Sohail100 <90067650+Abdullah-Sohail100@users.noreply.github.com>
2023-11-20 21:27:12 +0500

    Navbar Updated
    Mobile compatibility added plus added separate modules for mobile navbar and desktop navbar

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -1,13 +1,17 @@
 import React from "react";
 import Sidebar from "./Sidebar";
+import Navbar from "./Navbar";
 
 function MainPage() {
   return (
+    <>
+    <Navbar />
     <div className="flex items-center flex-row">
       <div>
         <Sidebar />
       </div>
     </div>
+    </>
   );
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,28 +1,48 @@
 import React from "react";
+import './Navbar.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
 
 const Navbar = () => {
   return (
-    <nav className="bg-gray-800 p-4">
-      <div className="container mx-auto">
-        <div className="flex items-center justify-between">
-          <div className="text-white font-bold text-xl">Bidding Bazaar</div>
-          <div className="space-x-4">
-            <a href="#" className="text-white hover:text-gray-300">
-              Home
-            </a>
-            <a href="#" className="text-white hover:text-gray-300">
-              About
-            </a>
-            <a href="#" className="text-white hover:text-gray-300">
-              Services
-            </a>
-            <a href="#" className="text-white hover:text-gray-300">
-              Contact
-            </a>
-          </div>
-        </div>
+    <nav>
+    <div id='RealNav'>
+      <div id="logo-container">
+        <h1 id="logo" ><b>BiddingBazaar</b></h1>
       </div>
-    </nav>
+      <div id="rightnav">
+        <button type="button" className="btn btn-outline-info" id="search-btn">🔍 Search &#9660;</button>&nbsp;&nbsp;
+        <ul id="navbar-list">
+          <li><a href="#">HOW IT WORKS</a></li>
+          <li>│</li>
+          <li><a href="#">LOG IN</a></li>
+          <li><a href="#">SIGN IN</a></li>
+          <li ><a href="#" style={{ color: 'orange' }}>FREELANCER?</a></li>
+        </ul>
+      </div>
+    </div>
+
+    <div id='mobileNav' style={{ backgroundColor: '#f9f9f9' }}>
+      <h1 id="logo" >BiddingBazaar</h1>
+      <button className="btn btn-primary" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight" aria-controls="offcanvasRight" id='MobileNabBarButton' >☰</button>
+    </div>
+
+    <div className="offcanvas offcanvas-end" tabIndex="-1" id="offcanvasRight" aria-labelledby="offcanvasRightLabel">
+      <div className="offcanvas-header">
+        <h5 className="offcanvas-title" id="offcanvasRightLabel"></h5>
+        <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+      </div>
+      <div className="offcanvas-body" style={{ padding: '20px' }}>
+        <p>🔍 Search</p>
+        <hr />
+        <p>HOW It WORKS</p><hr />
+        <p>LOG IN</p><hr />
+        <p>SIGN UP</p><hr />
+        <p style={{ color: 'orange' }}>FREELANCER?</p>
+      </div>
+    </div>
+  </nav>
   );
 };
 
