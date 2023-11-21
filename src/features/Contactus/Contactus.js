bcab9b6e326c9ddefa1f163bc250fbd1cb777cab
Abdullah-Sohail100 <90067650+Abdullah-Sohail100@users.noreply.github.com>
2023-11-21 23:46:05 +0500

    Social Media Icons Added in contact page

--- src/features/Contactus/Contactus.js
+++ src/features/Contactus/Contactus.js
@@ -1,5 +1,7 @@
 import React from "react";
 import bootstrapBundleMin from "bootstrap/dist/js/bootstrap.bundle.min";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTwitter, faFacebook, faInstagram } from '@fortawesome/free-brands-svg-icons';
 import Navbar from "../../components/Navbar";
 
 const ContactForm = () => {
@@ -41,10 +43,16 @@ const ContactForm = () => {
             </label>
             <textarea className="form-control" id="message" required />
           </div>
-          <button className="btn btn-danger" type="submit">
+          <button className="btn btn-danger"  style={{backgroundColor: "red"}}type="submit">
             {formStatus}
           </button>
         </form>
+        <div className="mt-4">
+          {/* Social media icons */}
+          <FontAwesomeIcon icon={faTwitter} size="2x" style={{ margin: '0 5px' }} />
+          <FontAwesomeIcon icon={faFacebook} size="2x" style={{ margin: '0 5px' }} />
+          <FontAwesomeIcon icon={faInstagram} size="2x" style={{ margin: '0 5px' }} />
+        </div>
       </div>
     </>
   );
