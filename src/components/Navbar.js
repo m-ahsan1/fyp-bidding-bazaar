294b0d8401fcb22a7e4eb387856c2567a198f72a
Uf11 <itsusman11@gmail.com>
2023-11-21 22:05:21 +0500

    User Login and Sign up

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,50 +1,81 @@
-import React from "react";
+import { React, useState, useEffect, useRef } from "react";
 import './Navbar.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import { Link } from "react-router-dom";
-
+import UserSign from "../features/UserSignup/UserSignup";
+import UserLogin from "../features/UserLogin/UserLogin";
 
 const Navbar = () => {
+  const [isUserSignOpen, setIsUserSign] = useState(false);
+  const [isUserLoginOpen, setIsUserLogin] = useState(false);
+  const OpenSignup = () => {
+    setIsUserSign(true);
+  };
+
+  const CloseSignup = () => {
+    setIsUserSign(false);
+  };
+
+  const OpenLogin = () => {
+    setIsUserLogin(true);
+  };
+
+  const CloseLogin = () => {
+    setIsUserLogin(false);
+  };
+
   return (
-    <nav>
-    <div id='RealNav'>
-      <div id="logo-container">
-       <Link to='/'> <h1  id="logo" ><b>BiddingBazaar</b></h1></Link>
+    <div>
+      <nav>
+      <div id='RealNav'>
+        <div id="logo-container">
+        <Link to='/' style={{ textDecoration: 'none' }}> <h1 id="logo" ><b>BiddingBazaar</b></h1></Link>
+        </div>
+        <div id="rightnav">
+          <button type="button" className="btn btn-outline-info" id="search-btn">🔍 Search &#9660;</button>&nbsp;&nbsp;
+          <ul id="navbar-list">
+            <li><a href="#">HOW IT WORKS</a></li>
+            <li>│</li>
+            <li><button onClick={OpenLogin} >Log In</button></li>
+            <li><button onClick={OpenSignup} >Signup</button></li>
+            <li ><Link to='/contact' style={{ color: 'orange' }}>Contact</Link></li>
+          </ul>
+        </div>
       </div>
-      <div id="rightnav">
-        <button type="button" className="btn btn-outline-info" id="search-btn">🔍 Search &#9660;</button>&nbsp;&nbsp;
-        <ul id="navbar-list">
-          <li><a href="#">HOW IT WORKS</a></li>
-          <li>│</li>
-          <li><a href="#">LOG IN</a></li>
-          <li><a href="#">SIGN IN</a></li>
-          <li ><Link to='/contact' style={{ color: 'orange' }}>Contact</Link></li>
-        </ul>
-      </div>
-    </div>
 
-    <div id='mobileNav' style={{ backgroundColor: '#f9f9f9' }}>
-      <h1 id="logo" >BiddingBazaar</h1>
-      <button className="btn btn-primary" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight" aria-controls="offcanvasRight" id='MobileNabBarButton' >☰</button>
-    </div>
+      <div id='mobileNav' style={{ backgroundColor: '#f9f9f9' }}>
+        <h1 id="logo" >BiddingBazaar</h1>
+        <button className="btn btn-primary" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight" aria-controls="offcanvasRight" id='MobileNabBarButton' >☰</button>
+      </div>
 
-    <div className="offcanvas offcanvas-end" tabIndex="-1" id="offcanvasRight" aria-labelledby="offcanvasRightLabel">
-      <div className="offcanvas-header">
-        <h5 className="offcanvas-title" id="offcanvasRightLabel"></h5>
-        <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+      <div className="offcanvas offcanvas-end" tabIndex="-1" id="offcanvasRight" aria-labelledby="offcanvasRightLabel">
+        <div className="offcanvas-header">
+          <h5 className="offcanvas-title" id="offcanvasRightLabel"></h5>
+          <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+        </div>
+        <div className="offcanvas-body" style={{ padding: '20px' }}>
+          <p>🔍 Search</p>
+          <hr />
+          <p>HOW It WORKS</p><hr />
+          <p>LOG IN</p><hr />
+          <p>SIGN UP</p><hr />
+          <p style={{ color: 'orange' }}>FREELANCER?</p>
+        </div>
       </div>
-      <div className="offcanvas-body" style={{ padding: '20px' }}>
-        <p>🔍 Search</p>
-        <hr />
-        <p>HOW It WORKS</p><hr />
-        <p>LOG IN</p><hr />
-        <p>SIGN UP</p><hr />
-        <p style={{ color: 'orange' }}>Contact</p>
-      </div>
-    </div>
-    
-  </nav>
+      
+    </nav>
+    {isUserSignOpen && (
+        <div>
+          <UserSign  onClose={CloseSignup}/>
+        </div>
+      )}
+      {isUserLoginOpen && (
+        <div>
+          <UserLogin  onClose={CloseLogin}/>
+        </div>
+      )}
+  </div>
   );
 };
 
---
+++ src/features/UserLogin/UserLogin.js
@@ -0,0 +1,114 @@
+import React, { useRef, useEffect } from "react";
+
+export default function UserLogin({onClose}) {
+        const loginRef = useRef(null);
+      
+        useEffect(() => {
+          const handleClickOutside = (event) => {
+            if (loginRef.current && !loginRef.current.contains(event.target)) {
+              onClose();
+            }
+          };
+      
+          document.addEventListener("mousedown", handleClickOutside);
+      
+          return () => {
+            document.removeEventListener("mousedown", handleClickOutside);
+          };
+        }, [onClose]);
+    return (
+        <div>
+            <div 
+            className="fixed inset-0 overflow-y-auto bg-black bg-opacity-50 flex items-center justify-center">
+                <div 
+                ref={loginRef}
+                className="bg-white p-10 min-h-30 max-w-md mx-auto rounded-lg shadow-md"
+                 style={{ minWidth: "35vw", maxHeight: "100vh" }}>
+                    <div 
+            style={{maxHeight: "100vh" }}
+                    className="flex flex-col items-center max-h-screen pt-6 sm:justify-center sm:pt-0 bg-gray-50">
+                        <div>
+                            <a href="/">
+                                <h3 className="text-4xl font-bold text-purple-600">
+                                    Logo
+                                </h3>
+                            </a>
+                        </div>
+                        <div className="w-full px-6 py-4 mt-6 overflow-hidden bg-white shadow-md sm:max-w-lg sm:rounded-lg">
+                            <form>
+                        
+                                <div className="mt-4">
+                                    <label
+                                        htmlFor="email"
+                                        className="block text-sm font-medium text-gray-700 undefined"
+                                    >
+                                        Email
+                                    </label>
+                                    <div className="flex flex-col items-start">
+                                        <input
+                                            type="email"
+                                            name="email"
+                                            className="block w-full mt-1 border-gray-300 rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                                        />
+                                    </div>
+                                </div>
+                                
+                                <div className="mt-4">
+                                    <label
+                                        htmlFor="password"
+                                        className="block text-sm font-medium text-gray-700 undefined"
+                                    >
+                                        Password
+                                    </label>
+                                    <div className="flex flex-col items-start">
+                                        <input
+                                            type="password"
+                                            name="password"
+                                            className="block w-full mt-1 border-gray-300 rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                                        />
+                                    </div>
+                                </div>
+                                
+                                
+                                <div className="flex items-center mt-4">
+                                    <button type="submit" className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-purple-700 rounded-md hover:bg-purple-600 focus:outline-none focus:bg-purple-600">
+                                        Login
+                                    </button>
+                                </div>
+                            </form>
+                            <div className="mt-4 text-grey-600" >
+                                Don't have a Account?{" "}
+                                <span>
+                                    <button className="text-blue-500 hover:underline" >Signup</button>
+                                </span>
+                            </div>
+                            <div className="flex items-center w-full my-4">
+                                <hr className="w-full" />
+                                <p className="px-3 ">OR</p>
+                                <hr className="w-full" />
+                            </div>
+                            <div className="my-6 space-y-2">
+                                <button
+                                    aria-label="Login with Google"
+                                    type="button"
+                                    className="flex items-center justify-center w-full p-2 space-x-4 border rounded-md focus:ring-2 focus:ring-offset-1 dark:border-gray-400 focus:ring-violet-400"
+                                >
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        viewBox="0 0 32 32"
+                                        className="w-5 h-5 fill-current"
+                                    >
+                                        <path d="M16.318 13.714v5.484h9.078c-0.37 2.354-2.745 6.901-9.078 6.901-5.458 0-9.917-4.521-9.917-10.099s4.458-10.099 9.917-10.099c3.109 0 5.193 1.318 6.38 2.464l4.339-4.182c-2.786-2.599-6.396-4.182-10.719-4.182-8.844 0-16 7.151-16 16s7.156 16 16 16c9.234 0 15.365-6.49 15.365-15.635 0-1.052-0.115-1.854-0.255-2.651z"></path>
+                                    </svg>
+                                    <p>Login with Google</p>
+                                </button>
+                                
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
