a5e973dcce9f07a137142e7b38e8f8b0205287bb
Abdullah-Sohail100 <90067650+Abdullah-Sohail100@users.noreply.github.com>
2023-11-21 19:10:08 +0500

    Admin Login added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import ListingsPage from "./features/Listing/ListingsPage";
 import MainPage from "./components/MainPage";
+import AdminLogin from "./features/Adminlogin/adminLogin"; 
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<MainPage />} />
         <Route path="/listings" element={<ListingsPage />} />
+        <Route path="/admin" element={<AdminLogin />} />
       </Routes>
     </Router>
   );
---
+++ src/features/Adminlogin/adminLogin.js
@@ -0,0 +1,68 @@
+import React, { useState } from 'react';
+import './adminLogin.css'; // Make sure to import your CSS file
+
+const AdminLogin = () => {
+  const [showPassword, setShowPassword] = useState(false);
+  
+  const handleTogglePassword = () => {
+    setShowPassword(!showPassword);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Add your login logic here or route to a login function
+    console.log('Login logic will go here');
+  };
+
+  return (
+    <div>
+      {/* Header */}
+      <header className="header">
+        <nav className="nav">
+          <a href="#" className="nav_logo">
+            BiddingBazzar
+          </a>
+        </nav>
+      </header>
+
+      {/* Home */}
+      <section className="home show">
+        <div className="form_container">
+          {/* Login Form */}
+          <div className="form login_form">
+            <form onSubmit={handleSubmit}>
+              <h2>Admin Login</h2>
+
+              <div className="input_box">
+                <input
+                  type="email"
+                  placeholder="Enter your email"
+                  required
+                />
+                <i className="uil uil-envelope-alt email"></i>
+              </div>
+
+              <div className="input_box">
+                <input
+                  type={showPassword ? 'text' : 'password'}
+                  placeholder="Enter your password"
+                  required
+                />
+                <i
+                  className={`uil ${showPassword ? 'uil-eye' : 'uil-eye-slash'} pw_hide`}
+                  onClick={handleTogglePassword}
+                ></i>
+              </div>
+
+              <button className="button" type="submit">
+                Login Now
+              </button>
+            </form>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default AdminLogin;
