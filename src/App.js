af18f4b8a7199939dc8d864552432209b7701fb0
Uf11 <itsusman11@gmail.com>
2023-11-21 22:58:29 +0500

    User Profile

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import MainPage from "./components/MainPage";
 import AdminLogin from "./features/Adminlogin/adminLogin"; 
 import ContactForm from "./features/Contactus/Contactus";
 import BlogEditor from "./features/WriteBlog/WriteBlog";
+import UserProfile from "./features/UserProfile/UserProfile";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
         <Route path="/admin" element={<AdminLogin />} />
         <Route path="/contact" element={<ContactForm />} />
         <Route path="/writeblog" element={<BlogEditor />} />
+        <Route path="/user-profile" element={<UserProfile />} />
       </Routes>
     </Router>
   );
