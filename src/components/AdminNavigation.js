21dc1c71786b8eb240ff42446d74b08e06a249d4
Abdullah-Sohail100 <90067650+Abdullah-Sohail100@users.noreply.github.com>
2023-12-03 02:13:46 +0500

    Update AdminNavigation.js

--- src/components/AdminNavigation.js
+++ src/components/AdminNavigation.js
@@ -13,13 +13,15 @@ function AdminNavigation() {
       <Link to="/adminhome">Logout</Link>
       </div>
 
+      
       <div className="content">
-        <br /><br/><br />
-
-        <h1>Analytics</h1>
-        
+              
+      <br /><br/><br />  
+      <h1 style={{color:'black', fontSize:"30px"}}>Analytics</h1>
+      <br />
         <p>See advance stats at <Link style={{color:'blue'}} to="https://analytics.google.com/analytics/web/#/p418197527/reports/reportinghub">google analytics</Link></p>
         <br />
+        <h1 style={{color:'black', fontSize:"30px"}}>System Stats</h1>
         <AnalyticsComponent />
       </div>
     </div>
