0c71473a7f91468e5c88e6b8d9e5d406d277a51e
Abdullah-Sohail100 <90067650+Abdullah-Sohail100@users.noreply.github.com>
2023-12-03 02:08:00 +0500

    System stats showing on dashboard

--- src/components/Analytics.js
+++ src/components/Analytics.js
@@ -13,6 +13,7 @@ function AnalyticsComponent() {
       })
       .catch((error) => {
         console.error(error);
+        setIsLoading(false); // Set loading to false on error
       });
   }, []);
 
@@ -20,14 +21,23 @@ function AnalyticsComponent() {
     return <p>Loading server stats...</p>;
   }
 
+  // Calculate CPU usage as an average of load averages
+  const cpuUsage = ((serverStats.cpuUsage.reduce((acc, val) => acc + val, 0) / serverStats.cpuUsage.length) * 100).toFixed(2);
+
+  // Calculate memory usage
+  const totalMemoryMB = (serverStats.memoryUsage.totalMemory / (1024 * 1024)).toFixed(2);
+  const freeMemoryMB = (serverStats.memoryUsage.freeMemory / (1024 * 1024)).toFixed(2);
+  const usedMemoryMB = (totalMemoryMB - freeMemoryMB).toFixed(2);
+  const memoryUsagePercentage = ((usedMemoryMB / totalMemoryMB) * 100).toFixed(2);
+
   return (
     <div>
       <h2>Server Resource Utilization</h2>
       <p>Active Users: {serverStats.activeUsers}</p>
-      <p>CPU Usage: {serverStats.cpuUsage[0].toFixed(2)}%</p>
-      <p>Memory Usage: {Math.round((serverStats.memoryUsage.used / serverStats.memoryUsage.total) * 100)}%</p>
-      <p>Total Memory: {serverStats.memoryUsage.total / 1024 / 1024} MB</p>
-      <p>Free Memory: {serverStats.memoryUsage.free / 1024 / 1024} MB</p>
+      <p>CPU Usage: {cpuUsage}%</p>
+      <p>Memory Usage: {memoryUsagePercentage}%</p>
+      <p>Total Memory: {totalMemoryMB} MB</p>
+      <p>Free Memory: {freeMemoryMB} MB</p>
       {/* Add additional visualizations or components based on your needs */}
     </div>
   );
