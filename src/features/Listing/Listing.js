a483dd38eed15687a0f7f36a72cdd39d9c8b1968
Muhammad Ahsan <ahsank0811@gmail.com>
2023-12-03 23:43:19 +0500

    added strip payment to listings

--- src/features/Listing/Listing.js
+++ src/features/Listing/Listing.js
@@ -1,4 +1,6 @@
+import axios from "axios";
 import React from "react";
+import StripeCheckout from "react-stripe-checkout";
 
 function Listing({
   image,
@@ -10,6 +12,26 @@ function Listing({
   description,
   company,
 }) {
+  const publishableKey =
+    "pk_test_51OJKAXLYINFqcfoRE0wdt2axn9TVcPLJMeGZzmFBavqw5c8x2xTSRqxnVsjuGMWZIWDsYT6M4MB7eW8bUPFRNy2Z00u3wQxOhi";
+  const payNow = async (token, price) => {
+    try {
+      const response = await axios({
+        url: "http:localhost:3001/api/payment",
+        method: "post",
+        data: {
+          amount: price,
+          token,
+        },
+      });
+
+      if (response.status === 200) {
+        console.log("Payment was sucessful!");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <div className=" w-[400px] overflow-hidder rounded shadow-lg">
       <img className="w-[400px] h-[200px]" src={image} alt="Listed Car"></img>
@@ -37,6 +59,16 @@ function Listing({
         <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
           {mileage}
         </span>
+        <StripeCheckout
+          stripeKey={publishableKey}
+          label="Pay"
+          name="Pay with Credit Card"
+          billingAddress
+          shippingAddress
+          amount={price}
+          description={"Your total is " + price}
+          token={payNow}
+        />
       </div>
     </div>
   );
