fe4218dfa97d445b13e4fa9793d84ee84aa0eb45
Abdullah-Sohail100 <90067650+Abdullah-Sohail100@users.noreply.github.com>
2024-06-07 21:00:32 +0500

    Update Subbar.js

--- Frontend/src/components/Subbar.js
+++ Frontend/src/components/Subbar.js
@@ -41,6 +41,7 @@ const Subbar = () => {
             More options
           </Dropdown.Toggle>
           <hr />
+          
           <Dropdown.Menu style={{ width: "100%" }}>
             <Dropdown.Item href="#"><Link to="/listings">Post Car</Link></Dropdown.Item>
             <Dropdown.Item href="#"><Link to="/blogs">Blogs</Link></Dropdown.Item>
