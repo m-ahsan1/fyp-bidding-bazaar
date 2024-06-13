6025381b5505917c43b7a043456292bb0609b532
Abdullah-Sohail100 <90067650+Abdullah-Sohail100@users.noreply.github.com>
2024-06-14 01:07:00 +0500

    pagination

--- Frontend/src/features/Auction/Main.js
+++ Frontend/src/features/Auction/Main.js
@@ -12,7 +12,9 @@ export const Main = () => {
   const { globalMsg } = useContext(AuthContext);
   const { docs } = useFirestoreCollection("auctions");
   const [showEnded, setShowEnded] = useState(false); // State to track whether to show ended auctions
-  // console.log(docs);
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 6;
+
   // Filter active or ended auctions based on the showEnded state
   const filteredAuctions =
     docs &&
@@ -22,30 +24,63 @@ export const Main = () => {
         : doc.status === "active"
     );
 
+  // Calculate pagination details
+  const totalPages = Math.ceil(filteredAuctions.length / itemsPerPage);
+  const startIdx = (currentPage - 1) * itemsPerPage;
+  const endIdx = startIdx + itemsPerPage;
+  const currentItems = filteredAuctions.slice(startIdx, endIdx);
+
+  const handleNextPage = () => {
+    if (currentPage < totalPages) setCurrentPage(currentPage + 1);
+  };
+
+  const handlePrevPage = () => {
+    if (currentPage > 1) setCurrentPage(currentPage - 1);
+  };
+
   return (
     <div className="py-5">
       <div className="container mx-auto px-4">
         {auction && <ProgressBar auction={auction} setAuction={setAuction} />}
         {auth.currentUser && <AddAuction setAuction={setAuction} />}
-        {globalMsg && toast.success(globalMsg, {
-          position: toast.POSITION.TOP_CENTER,
-        })}
+        {globalMsg &&
+          toast.success(globalMsg, {
+            position: toast.POSITION.TOP_CENTER,
+          })}
 
         <div className="flex justify-center mt-4">
           <button
             onClick={() => setShowEnded(!showEnded)}
             className="btn btn-primary"
           >
-            {showEnded ? "Show Active Auctions" : "Show Your auctions"}
+            {showEnded ? "Show Active Auctions" : "Show Your Auctions"}
           </button>
         </div>
-        {filteredAuctions && (
+
+        {currentItems && (
           <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 mt-4">
-            {filteredAuctions.map((doc) => {
+            {currentItems.map((doc) => {
               return <AuctionCard item={doc} key={doc.id} />;
             })}
           </div>
         )}
+
+        <div className="flex justify-center mt-4">
+          <button
+            onClick={handlePrevPage}
+            disabled={currentPage === 1}
+            className="btn btn-secondary mx-2"
+          >
+            Previous
+          </button>
+          <button
+            onClick={handleNextPage}
+            disabled={currentPage === totalPages}
+            className="btn btn-secondary mx-2"
+          >
+            Next
+          </button>
+        </div>
       </div>
     </div>
   );
